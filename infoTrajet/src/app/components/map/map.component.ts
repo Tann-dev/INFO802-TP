d9792c66be3506dbfb6220c465357146133b62b8
Tan <75690349+Tann-dev@users.noreply.github.com>
2023-03-16 08:40:39 +0100

    Update map.component.ts

--- infoTrajet/src/app/components/map/map.component.ts
+++ infoTrajet/src/app/components/map/map.component.ts
@@ -89,11 +89,9 @@ export class MapComponent {
 
     this.findWaypoints().then((result: any) => {
       this.waypoints.push(L.latLng(trajet.depart.lat, trajet.depart.lon))
-      if(result.length != 0) {
-        result.forEach((ele: { lat: number; lng: number; }) => {
-          this.waypoints.push(L.latLng(ele.lat, ele.lng))
-        });
-      }
+      result.forEach((ele: { lat: number; lng: number; }) => {
+        this.waypoints.push(L.latLng(ele.lat, ele.lng))
+      });
       this.waypoints.push(L.latLng(trajet.arrivee.lat, trajet.arrivee.lon))
 
       this.itineraire.setWaypoints(this.waypoints)
@@ -136,8 +134,8 @@ export class MapComponent {
               });
             }
           }
-          resolve(bornes);
         }
+        resolve(bornes);
       })
     });
   }
